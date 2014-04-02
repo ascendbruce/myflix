157e736331375db1e9eed0aacb722d045dfed764
Bruce Li <ascendbruce@gmail.com>
2014-04-03 00:40:12 +0800

    Refactoring: change rating in the queue

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,17 @@ class User < ActiveRecord::Base
       queue_item.update_attributes(position: index + 1) if queue_item
     end
   end
+
+  def queue_items_update_rating_by(queue_item_and_ratings)
+    return if queue_item_and_ratings.blank?
+    queue_item_and_ratings.each do |queue_item_id, rating|
+      queue_item = queue_items.find_by_id(queue_item_id)
+
+      if queue_item
+        review = Review.where(user: self, video: queue_item.video).first_or_create
+        review.update_attributes(skip_validates_presence_of_comment: true, rating: rating)
+      end
+    end
+  end
+
 end
