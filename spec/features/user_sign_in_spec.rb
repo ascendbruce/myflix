c6b484a3ba9a81c477aa3b30315ce4003920c530
Bruce Li <ascendbruce@gmail.com>
2014-04-05 14:17:26 +0800

    Implement the first feature spec (user sign in)

---
+++ spec/features/user_sign_in_spec.rb
@@ -0,0 +1,32 @@
+require "spec_helper"
+
+feature "Signing in" do
+  given!(:user) { Fabricate(:user) }
+
+  scenario "Signing in with correct credentials" do
+    visit sign_in_path
+
+    within("form.sign_in") do
+      fill_in "email",    with: user.email
+      fill_in "password", with: user.password
+    end
+
+    click_button "Sign in"
+
+    expect(page).to have_content "You are signed in"
+    expect(page).to have_content user.full_name
+  end
+
+  scenario "Signing in with wrong credentials" do
+    visit sign_in_path
+
+    within("form.sign_in") do
+      fill_in "email",    with: user.email
+      fill_in "password", with: "xxx#{user.password}"
+    end
+
+    click_button "Sign in"
+
+    expect(page).to have_content "Invalid email or password"
+  end
+end
