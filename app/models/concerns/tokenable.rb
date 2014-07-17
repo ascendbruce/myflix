6d6741caa73f5a185f2c63c2b01227ec28d0b6b3
Bruce Li <ascendbruce@gmail.com>
2014-07-17 21:21:13 +0800

    Remove redundant self. in concerns

--- app/models/concerns/tokenable.rb
+++ app/models/concerns/tokenable.rb
@@ -4,6 +4,6 @@ module Tokenable
   extend ActiveSupport::Concern
 
   def generate_token
-    self.update_attribute(:token, SecureRandom.urlsafe_base64)
+    update_attribute(:token, SecureRandom.urlsafe_base64)
   end
 end
