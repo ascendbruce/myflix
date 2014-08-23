5a9872272b0af28e1583533bfcec84d7bd5ebe61
Bruce Li <ascendbruce@gmail.com>
2014-08-23 18:02:26 +0800

    Admin adds a video (without cover)

--- spec/controllers/admin/videos_controller_spec.rb
+++ spec/controllers/admin/videos_controller_spec.rb
@@ -14,19 +14,69 @@ describe Admin::VideosController do
       expect(assigns(:video)).to be_new_record
     end
 
-    context "for regular users" do
+    it_behaves_like "require_admin" do
+      let(:action) { get :new }
+    end
+
+    it "sets the flash error message" do
+      set_current_user
+      get :new
+      expect(flash[:error]).to be_present
+    end
+  end
+
+  describe "POST 'create'" do
+    it_behaves_like "require_signed_in" do
+      let(:action) { post "create" }
+    end
+
+    it_behaves_like "require_admin" do
+      let(:action) { post "create" }
+    end
+
+    context "with valid input" do
+      let!(:category) { Fabricate(:category) }
+
       before do
-        set_current_user
+        set_current_admin
+        post :create, video: { title: "Monk", description: "Monk", category_id: category.id }
       end
 
-      it "redirects to the home path" do
-        get :new
-        expect(response).to redirect_to home_path
+      it "redirects to add new videos page" do
+        expect(response).to redirect_to new_admin_video_path
+      end
+
+      it "creates a video" do
+        expect(category.videos.count).to eq 1
+      end
+
+      it "sets the flash success message" do
+        expect(flash[:success]).to eq "You have successfully added the video 'Monk'"
+      end
+    end
+
+    context "with invalid input" do
+      let!(:category) { Fabricate(:category) }
+
+      before do
+        set_current_admin
+        post :create, video: { description: "Monk", category_id: category.id }
+      end
+
+      it "does not create a video" do
+        expect(category.videos.count).to eq 0
+      end
+
+      it "renders the :new template" do
+        expect(response).to render_template :new
+      end
+
+      it "sets the @video" do
+        expect(assigns(:video)).to be_present
       end
 
       it "sets the flash error message" do
-        get :new
-        expect(flash[:error]).to be_present
+        expect(flash[:error]).to eq "you cannot add this video. Please check the errors."
       end
     end
   end
