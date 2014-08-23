50d9b253bbaaea09cc98cf87b946d885ae6b73be
Bruce Li <ascendbruce@gmail.com>
2014-08-24 01:58:16 +0800

    Fix timeout problem in heroku

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,9 +4,10 @@ CarrierWave.configure do |config|
     config.fog_credentials = {
       :provider               => "AWS",
       :aws_access_key_id      => ENV["AWS_ACCESS_KEY_ID"],
-      :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"]
+      :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"],
+      :region                 => 'us-west-2'
     }
-    config.fog_directory  = 'ascendbruce-myflix'
+    config.fog_directory  = 'ascendbruce-myflix-oregon'
   else
     config.storage :file
     config.enable_processing = Rails.env.development?
