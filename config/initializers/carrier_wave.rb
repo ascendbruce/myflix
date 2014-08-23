ed264bbbb20b198ea5675c07d880969eade0a8d1
Bruce Li <ascendbruce@gmail.com>
2014-08-24 01:36:53 +0800

    ascendbruce-myflix-oregon

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,12 +1,13 @@
 CarrierWave.configure do |config|
   if Rails.env.production? || Rails.env.production?
+    config.cache_dir = "#{Rails.root}/tmp/uploads"
     config.storage = :fog
     config.fog_credentials = {
       :provider               => "AWS",
       :aws_access_key_id      => ENV["AWS_ACCESS_KEY_ID"],
       :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"]
     }
-    config.fog_directory  = 'ascendbruce-myflix'
+    config.fog_directory  = 'ascendbruce-myflix-oregon'
   else
     config.storage :file
     config.enable_processing = Rails.env.development?
