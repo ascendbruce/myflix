05ae3c5f033319d079b7aba547b68929bc293617
Bruce Li <ascendbruce@gmail.com>
2014-09-01 00:24:03 +0800

    Revert "compile assets in production" and set precompile assets instead

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,8 +9,9 @@ Myflix::Application.configure do
 
   config.assets.compress = true
   config.assets.js_compressor = :uglifier
+  config.assets.precompile += %w(payment.js)
 
-  config.assets.compile = true
+  config.assets.compile = false
 
   config.assets.digest = true
 
