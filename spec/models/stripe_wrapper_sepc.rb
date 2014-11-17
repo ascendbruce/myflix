187c943f22cbbcac280774592bb5090409eed0ea
Bruce Li <ascendbruce@gmail.com>
2014-11-17 21:49:51 +0800

    Refactor improving reusability in stripe_wrapper_spec

--- spec/models/stripe_wrapper_sepc.rb
+++ spec/models/stripe_wrapper_sepc.rb
@@ -2,59 +2,57 @@ require 'spec_helper'
 require "stripe"
 
 describe StripeWrapper do
+  let(:valid_token) do
+    Stripe::Token.create(
+      :card => {
+        :number    => "4242424242424242",
+        :exp_month => 9,
+        :exp_year  => Time.now.year+1,
+        :cvc       => "314"
+      },
+    ).id
+  end
+
+  let(:declined_token) do
+    Stripe::Token.create(
+      :card => {
+        :number    => "4000000000000002",
+        :exp_month => 9,
+        :exp_year  => Time.now.year+1,
+        :cvc       => "314"
+      },
+    ).id
+  end
+
   describe StripeWrapper::Charge do
-    describe ".create" do
-      it "makes a successful charge", :vcr do
-        token = Stripe::Token.create(
-          :card => {
-            :number    => "4242424242424242",
-            :exp_month => 9,
-            :exp_year  => Time.now.year+1,
-            :cvc       => "314"
-          },
-        ).id
+
+    describe ".create", :vcr do
+      it "makes a successful charge" do
 
         response = StripeWrapper::Charge.create(
           :amount      => 999,
-          :card        => token,
+          :card        => valid_token,
           :description => "test"
         )
 
         expect(response).to be_successful
       end
 
-      it "makes a card declined charge", :vcr do
-        token = Stripe::Token.create(
-          :card => {
-            :number    => "4000000000000002",
-            :exp_month => 9,
-            :exp_year  => Time.now.year+1,
-            :cvc       => "314"
-          },
-        ).id
+      it "makes a card declined charge" do
 
         response = StripeWrapper::Charge.create(
           :amount      => 999,
-          :card        => token,
+          :card        => declined_token,
           :description => "test"
         )
 
         expect(response).not_to be_successful
       end
 
-      it "returns the error message for declined charge", :vcr do
-        token = Stripe::Token.create(
-          :card => {
-            :number    => "4000000000000002",
-            :exp_month => 9,
-            :exp_year  => Time.now.year+1,
-            :cvc       => "314"
-          },
-        ).id
-
+      it "returns the error message for declined charge" do
         response = StripeWrapper::Charge.create(
           :amount      => 999,
-          :card        => token,
+          :card        => declined_token,
           :description => "test"
         )
 
